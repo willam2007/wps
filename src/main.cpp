b802535cdbc8f30819d8b97794ed740c4143478e
CuteBanMachine <warfaceart@gmail.com>
2024-10-10 00:59:58 +1000

    to my branch

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow/mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "wps_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    
+    MainWindow w;
+    w.setWindowState(Qt::WindowMaximized);
+    w.show();
+    return a.exec(); 
+}
